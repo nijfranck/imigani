458c620cca1d821942a180192a8f9a2a50016b7a
Franck Nijimbere <nijimbere.franck@gmail.com>
2018-02-02 18:58:19 -0500

    adding imigani

---
+++ imigani.js
@@ -0,0 +1,11 @@
+module.exports = {
+	imigani: [
+		"Intwengo y'ûmwâna ihimbâra nyina",
+		"Ababúranâma bīcīsha abajânâma.",
+		"Abafíse amênyo baraseka (bōtwenga, bōseka).",
+		"Abagabo babiri ntíbabāna mu nzu.",
+		"Abagabo b'ubunyegeri bagānīrira kw'igúfa.",
+		"Abagabo ntibaronkéra rimwé (baba básemana).",
+		"Abaséka bâritse ntíbugwirá",
+	]
+}
